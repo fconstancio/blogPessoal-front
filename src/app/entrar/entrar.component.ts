76574ca15820a80a98c6c7ffa08bf997b296be3e
fconstancio <constanciosalomao@gmail.com>
2021-06-29 11:37:21 -0300

    consumo de login e cadastro

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private route: Router) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
   }
+
+  entrar() {
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
+      this.userLogin = resp
+
+      environment.id = this.userLogin.id
+      environment.nome = this.userLogin.nome
+      environment.token = this.userLogin.token
+      environment.foto = this.userLogin.foto
+
+      this.route.navigate(['/inicio'])
+      
+    }, erro => {
+      if (erro.status == 401) {
+        console.log('Usuario ou Senha incorreta')
+      }
+    })
+  }
+
 
 }
