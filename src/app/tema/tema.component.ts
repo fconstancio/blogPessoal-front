855a623a4adc56acdd6fd40913a871512598e06e
fconstancio <constanciosalomao@gmail.com>
2021-06-30 12:59:11 -0300

    componente tema sem consumo

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
 })
-export class InicioComponent implements OnInit {
+export class TemaComponent implements OnInit {
   
+  faAngleDoubleLeft = faAngleDoubleLeft
+
   constructor(
     private router: Router
   ) { }
 
   ngOnInit() {
+
     if (environment.token == '') {
       this.router.navigate(['/entrar'])
+
     }
   }
 
